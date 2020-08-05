f88e64fb15ddc3cfa1356686afaba7d372e52b40
Roland Levy <rolandjlevy@gmail.com>
2020-08-05 09:15:09 +0000

    Refactored InputHandler

--- src/InputHandler.js
+++ src/InputHandler.js
@@ -1,27 +1,31 @@
 class InputHandler {
   constructor(area, balls) {
-  ['mousedown', 'touchstart'].forEach(event => {
-    area.canvas.addEventListener(event, (e) => { 
-      const canvasRect = e.target.getBoundingClientRect();
-      const event = e.touches ? e.touches[0] : e;
-      const mouseX = event.clientX - canvasRect.left;
-      const mouseY = event.clientY - canvasRect.top;
-      balls.forEach(ball => {
-        if (mouseX >= ball.x - ball.width 
-         && mouseX <= ball.x + ball.width) {
-          ball.direction = -5;
-          ball.opacity = 1;
-        }
+    ['mousedown', 'touchstart'].forEach(event => {
+      area.canvas.addEventListener(event, (e) => {
+        const mousePos = this.mouseXY(e);
+        balls.forEach(ball => {
+          if (mousePos.x >= ball.x - ball.width 
+          && mousePos.x <= ball.x + ball.width) {
+            ball.direction = -5;
+            ball.opacity = 1;
+          }
+        })
       })
-    })
-  });
-  ['mouseup', 'touchend'].forEach(event => {
-    area.canvas.addEventListener(event, (e) => { 
-      balls.forEach(ball => { 
-        ball.direction = 1;
-        ball.opacity = 0.7;
-      });
-    })
-  });
+    });
+    ['mouseup', 'touchend'].forEach(event => {
+      area.canvas.addEventListener(event, (e) => { 
+        balls.forEach(ball => { 
+          ball.direction = 1;
+          ball.opacity = 0.7;
+        });
+      })
+    });
+  }
+  mouseXY(e) {
+    const canvasRect = e.target.getBoundingClientRect();
+    const eventType = e.touches ? e.touches[0] : e;
+    const x = eventType.clientX - canvasRect.left;
+    const y = eventType.clientY - canvasRect.top;
+    return { x, y };
   }
 }
